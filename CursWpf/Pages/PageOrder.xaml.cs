61328663bceb06b6603bf933724b029a0a984ec5
yowka <stasskirnevskiy@gmail.com>
2025-04-11 17:45:54 +0700

    all

--- CursWpf/Pages/PageOrder.xaml.cs
+++ CursWpf/Pages/PageOrder.xaml.cs
@@ -32,12 +32,19 @@ namespace CursWpf.Pages
 
             var order = (Order)ListUser.SelectedItem;
 
+            var sale = new Sale_automobile
+            {
+                date = DateTime.Now,
+                Automobile_ID = order.Automobile_ID,
+                Employee_ID = DBManager.id_employee,
+                Buyer_ID = order.Buyer_id,
+            };
+
+            DBManager.db.Sale_automobile.Add(sale);
+            DBManager.db.Order.Remove(order);
             DBManager.db.SaveChanges();
 
-            var items = (List<Order>)ListUser.ItemsSource;
-            items.Remove(order);
-            ListUser.ItemsSource = null;
-            ListUser.ItemsSource = items;
+            LoadOrders();
 
             MessageBox.Show("Заявка принята и удалена из списка!");
         }
@@ -48,12 +55,10 @@ namespace CursWpf.Pages
 
             var order = (Order)ListUser.SelectedItem;
 
+            DBManager.db.Order.Remove(order);
             DBManager.db.SaveChanges();
 
-            var items = (List<Order>)ListUser.ItemsSource;
-            items.Remove(order);
-            ListUser.ItemsSource = null;
-            ListUser.ItemsSource = items;
+            LoadOrders(); 
 
             MessageBox.Show("Заявка отклонена и удалена из списка!");
         }
